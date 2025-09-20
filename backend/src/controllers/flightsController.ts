fae902119787eeb0441bbcdb2d762e870c8eaff3
ludson96 <ludson_ps27@hotmail.com>
2025-09-20 10:32:04 -0300

    refactor: :recycle: refactor code

--- backend/src/controllers/flightsController.ts
+++ backend/src/controllers/flightsController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import * as flightService from '../services/flightsService';
 
-export const listFlights = (req: Request, res: Response) => {
+export const listFlights = (_req: Request, res: Response) => {
   const flights = flightService.getAllFlights();
   res.json(flights);
 };
