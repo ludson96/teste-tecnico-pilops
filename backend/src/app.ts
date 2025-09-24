660e1e83c49896c4f972a8b8296c6c2f8db72416
ludson96 <ludson_ps27@hotmail.com>
2025-09-24 10:09:31 -0300

    feat: :wrench: add cors and conect with backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 import flightsRoutes from './routes/flightsRoutes.js';
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,5 @@
 import app from './app.js';
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3001;
 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
