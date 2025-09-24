9ab993fe2d63cae7a6b240e23a2660d6c1e5a5ae
ludson96 <ludson_ps27@hotmail.com>
2025-09-24 09:44:54 -0300

    fix: :lipstick: fix style padding

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,10 +2,8 @@ import Header from "@/components/Header";
 
 export default function Home() {
   return (
-    <main className="bg-[#1A1A1A] min-h-screen p-4 sm:p-8">
-      <div className="max-w-7xl mx-auto w-full">
-        <Header />
-      </div>
+    <main className="bg-[#1A1A1A] min-h-screen p-[63px]">
+      <Header />
     </main>
   );
 }
