c8b7de12772f8287fb5c9e97a87eca98dba9b616
ludson96 <ludson_ps27@hotmail.com>
2025-09-24 09:30:41 -0300

    feat: :sparkles: add Header and render

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -2,24 +2,25 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <header className="text-center py-12">
+    <header>
 
       <Image
         src="/logo-pilops.svg"
         alt="Pilops Logo"
-        width={197.43}
-        height={57.47}
+        width={292}
+        height={89.27070617675781}
         className="mx-auto"
       />
 
-      <p className="font-manrope font-normal text-[15px] leading-[140%]">Your virtual pilot career for Flight Simulator</p>
+      <div className="w-[468px] h-[58px] absolute top-[214px] left-[66px] font-sora">
 
-      <h2 className="text-4xl font-bold">Histórico de Voos</h2>
+        <h2 className="font-bold text-[24px] leading-[110.00000000000001%] text-[#FFFFFF] mb-3">Histórico de Voos</h2>
 
-      <p className="text-gray-400 mt-2 mb-8">Visualize seu histórico completo de voos realizados</p>
+        <p className="font-normal text-[18px] leading-[110.00000000000001%] text-[#E0E0E0]">Visualize seu histórico completo de voos realizados</p>
+
+      </div>
 
     </header>
   );
 }
-
-// font-family: Manrope;
+// background: #E0E0E0;
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
       fontFamily: {
         sans: ["var(--font-geist-sans)"],
         mono: ["var(--font-geist-mono)"],
-        manrope: ["var(--font-manrope)"],
+        sora: ["var(--font-sora)"],
       },
     },
   },
